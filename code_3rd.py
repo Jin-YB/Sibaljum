185aaccddf6af4b47508596f15ab8d43a53b5b3d
jun <ddw0511@gmail.com>
2021-08-26 17:41:42 +0900

    3번 코드 영어및 숫자 기호 구현,시뮬레이션 회로까지

--- code_3rd.py
+++ code_3rd.py
@@ -1,10 +1,10 @@
 # import Runcode # 파일간 연결
 
 # result_data = Runcode.result_join
-result_data = "A각a"  # 테스트용
+result_data = "A깍a1,"  # 테스트용
 data = list(result_data)  #
 
-print(data)
+# print(data)
 uni_data = []
 count = len(data)
 cho_uni = []
@@ -12,7 +12,13 @@ joong_uni = []
 jong_uni = []
 eng_uni_c = []
 eng_uni_s = []
+all_list = [[-1 for col_n in range(3)] for row_n in range(3)]
+#  list_len = len(all_list)
 
+# all_list[A][B]
+# [0][0]=한글.초성 [0][1]=한글.중성 [0][2]=한글.종성
+# [1][0]=알파벳.대문자 [1][1]=알파벳.소문자
+# [2][0]=아라비아 숫자 [2][1]=기호
 
 def uni_set():
     for i in range(count):
@@ -23,12 +29,15 @@ def trans_uni_ko(seq):
     cho_uni.append((uni_data[seq] - 44032) // 588)  # 초성
     joong_uni.append(((uni_data[seq] - 44032) % 588) // 28)  # 중성
     jong_uni.append(((uni_data[seq] - 44032) % 588) % 28)  # 종성
+    all_list[0][0] = cho_uni.pop()
+    all_list[0][1] = joong_uni.pop()
+    all_list[0][2] = jong_uni.pop()
     #  여기부터
-    print(uni_data[seq])
+    """print(uni_data[seq])
     print(cho_uni.pop())
     print(joong_uni.pop())
     print(jong_uni.pop())
-    #  여기까지 테스트용 출력
+    #  여기까지 테스트용 출력"""
     #  출력시 저장되는 데이터 정보
     #  cho_uni (0~17){ 'ㄱ','ㄲ','ㄴ','ㄷ','ㄸ','ㄹ','ㅁ','ㅂ','ㅃ','ㅅ','ㅆ','ㅇ','ㅈ','ㅉ','ㅊ','ㅋ','ㅌ','ㅍ','ㅎ'}
     #  joong_uni (0~20){'ㅏ','ㅐ','ㅑ','ㅒ','ㅓ','ㅔ','ㅕ','ㅖ','ㅗ','ㅘ','ㅙ','ㅚ','ㅛ','ㅜ','ㅝ','ㅞ','ㅟ','ㅠ','ㅡ','ㅢ','ㅣ'}
@@ -37,13 +46,14 @@ def trans_uni_ko(seq):
 
 def trans_uni_en_c(seq):
     eng_uni_c.append(uni_data[seq] - 65)  # 대문자
-    print(eng_uni_c.pop())
+    #  print(eng_uni_c.pop())
+    all_list[1][0] = eng_uni_c.pop()
 
 
 def trans_uni_en_s(seq):
     eng_uni_s.append(uni_data[seq] - 97)  # 소문자
-    print(eng_uni_s.pop())
-
+    #  print(eng_uni_s.pop())
+    all_list[1][1] = eng_uni_s.pop()
 
 
 
--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import code_3rd
 import dot_data
+import dot_data_eng
 import circuit
 
 def start_data():
@@ -10,17 +11,31 @@ def start_data():
         if 44032 <= code_3rd.uni_data[stk] <= 55203:  # 유니코드 한글 범위
             code_3rd.trans_uni_ko(stk)
             dot_data.cho()
+            circuit.play()
             dot_data.joong()
+            circuit.play()
             dot_data.jong()
-            circuit.play(stk)
+            circuit.play()
+            dot_data.reset()
 
-        if 65 <= code_3rd.uni_data[stk] <= 90:  # 유니코드 대문자 영어 범위
+        elif 65 <= code_3rd.uni_data[stk] <= 90:  # 유니코드 대문자 영어 범위
             code_3rd.trans_uni_en_c(stk)
+            dot_data_eng.eng_c()
+            circuit.play()
 
-        if 97 <= code_3rd.uni_data[stk] <= 122:  # 유니코드 소문자 영어 범위
+        elif 97 <= code_3rd.uni_data[stk] <= 122:  # 유니코드 소문자 영어 범위
             code_3rd.trans_uni_en_s(stk)
+            dot_data_eng.eng_c()
+            circuit.play()
 
+        elif 48 <= code_3rd.uni_data[stk] <= 57:
+            dot_data_eng.number()
+            circuit.play()
+        else:
+            dot_data_eng.sign()
+            circuit.play()
         stk += 1
+
 
 
 start_data()
