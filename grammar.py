0fb7f3e059ba6369af04c68c550b4a5931374b7f
jun <ddw0511@gmail.com>
2021-08-28 17:39:33 +0900

    점자문법 제 11항 (테스트완료) +숫자 수정

--- grammar.py
+++ grammar.py
@@ -1,11 +1,13 @@
 import code_3rd
 
-
 grammar_10_uni = []
 grammar_10_count = [0] * code_3rd.count
+grammar_11_uni1 = []
+grammar_11_count = [0] * code_3rd.count
 
 
 def grammar_10():
+
     for i in range(code_3rd.count):
         grammar_10_uni.append(((code_3rd.uni_data[i] - 44032) % 588) % 28)
     a = 0
@@ -18,10 +20,31 @@ def grammar_10():
             break
 
         if grammar_10_uni[a] == 0:
-            if code_3rd.uni_data[b] == 50696:
+            if 50726 >= code_3rd.uni_data[b] >= 50696:
                 grammar_10_count[a] = 1
             else:
                 grammar_10_count[a] = 0
+        a += 1
+
+
+def grammar_11():
+    for i in range(code_3rd.count):
+        grammar_11_uni1.append(((code_3rd.uni_data[i] - 44032) % 588) % 28)
+
+    a = 0
+    while a < code_3rd.count:
+        b = a + 1
+        if code_3rd.count - 1 == a:
+            break
+
+        if grammar_11_uni1[a] == 0:
+            for i in range(24):
+                if code_3rd.uni_data[a] == (44088 + (588 * i)) or code_3rd.uni_data[a] == (44284 + (588 * i)) or \
+                        code_3rd.uni_data[a] == (44396 + (588 * i)) or code_3rd.uni_data[a] == (44424 + (588 * i)):
+                    if 50555 >= code_3rd.uni_data[b] >= 50528:  # 애 ~ 앻
+                        grammar_11_count[a] = 1
+                    else:
+                        grammar_11_count[a] = 0
         a += 1
 
 
@@ -30,14 +53,16 @@ def grammar_10():
         print(0)
 
 """
-
+# 테스트용
 code_3rd.uni_set()
 grammar_10()
-# 테스트용
+grammar_11()
+
 for i in range(code_3rd.count):
     if grammar_10_count[i] == 1:
         print(i)
+    elif grammar_11_count[i] == 1:
+        print(i)
     else:
         print("X")
-        
 
--- test.py
+++ test.py
@@ -1,6 +1,33 @@
-array = [[-1 for col in range(11)] for row in range(10)]
-print(array[3][1])
-if array[3][1] == 1:
-    print("d")
-elif array[3][1] == 0:
-    print("i")
+import code_3rd
+
+
+code_3rd.uni_set()
+# 12항 약자
+
+for i in range(code_3rd.count):
+    grammar_11_uni1.append(((code_3rd.uni_data[i] - 44032) % 588) % 28)
+
+a = 0
+while a < code_3rd.count:
+    b = a + 1
+    if code_3rd.count - 1 == a:
+        break
+
+    if grammar_11_uni1[a] == 0:
+        for i in range(24):
+            if code_3rd.uni_data[a] == (44088+(588*i)) or code_3rd.uni_data[a] == (44284+(588*i)) or code_3rd.uni_data[a] == (44396+(588*i)) or code_3rd.uni_data[a] == (44424+(588*i)):
+                if 50555 >= code_3rd.uni_data[b] >= 50528:  # 애 ~ 앻
+                    grammar_11_count[a] = 1
+                else:
+                    grammar_11_count[a] = 0
+    a += 1
+
+
+
+
+# 테스트용
+for i in range(code_3rd.count):
+    if grammar_11_count[i] == 1:
+        print(i)
+    else:
+        print("X")
